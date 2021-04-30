2562747bf7506be7b1797de31d6939100c1c8097
felipemimoura <felipemimoura@gmail.com>
2021-04-30 14:03:02 -0300

    criada a lógica para a pontuação conforme cada uma das jogadas

--- build/index.js
+++ build/index.js
@@ -1,24 +1,24 @@
 "use strict";
-// const sessao: Array<[]> = []
-// const jogadas: Object = {
-//   primeiraJogada: number,
-//   segundaJogada: Number | null
-// }
 class JogoDeBoliche {
     constructor() {
-        this.rolls = [];
+        this.pontos = 0;
+        this.jogadas = Array(21).fill(0);
+        this.JogadaAtual = 0;
     }
     Jogar(pinos) {
-        this.rolls.push(pinos);
+        this.jogadas[this.JogadaAtual++] = pinos;
     }
+    ;
     ObterPontuacao() {
-        let resultado = 0;
-        for (let i = 0; i < 20; i++) {
-            resultado += this.rolls[i];
+        let pontuacao = 0;
+        for (let i = 0; i < this.jogadas.length; i++) {
+            pontuacao += this.jogadas[i];
         }
-        return resultado;
+        return pontuacao;
     }
 }
 const jogo = new JogoDeBoliche();
 jogo.Jogar(5);
-jogo.ObterPontuacao();
+jogo.Jogar(5);
+jogo.Jogar(3);
+console.log(jogo.ObterPontuacao());
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
-
 class JogoDeBoliche {
-  public rolls = []
+  private pontos: number = 0
+  private jogadas: number[] = Array(21).fill(0)
+  private JogadaAtual: number = 0
 
   Jogar(pinos: number): void {
-  
-
+    this.jogadas[this.JogadaAtual++] = pinos
   };
 
-  ObterPontuacao(): number {}
+  ObterPontuacao(): number {
+   let pontuacao = 0
+   for(let i = 0; i< this.jogadas.length; i++){
+     pontuacao += this.jogadas[i]
+   }
+   return pontuacao
+  }
 }
 
+
+const jogo = new JogoDeBoliche()
+jogo.Jogar(5)
+jogo.Jogar(5)
+jogo.Jogar(3)
+console.log(jogo.ObterPontuacao())
